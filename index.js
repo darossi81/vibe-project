9881b6033687aab2ae0fa73d88be0672d0640d0d
Darren Rossiter <darrenrossiter@darrens-imac.home>
2024-08-16 12:02:02 +0100

    Set up basic Express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, Vibe Project!");
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello, Vibe Project!');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
